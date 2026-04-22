f158f9bac47890bcaf7be8c8867076f762674990
palstn97 <palstn97@naver.com>
2026-04-22 15:17:31 +0900

    초기 백엔드 프로젝트 세팅

---
+++ src/main/java/com/sportsai/sports_platform/SportsPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.sportsai.sports_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsPlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsPlatformApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sportsai/sports_platform/SportsPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sportsai.sports_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportsPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
